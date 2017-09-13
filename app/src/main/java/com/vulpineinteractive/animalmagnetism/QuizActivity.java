8a3cb9f059e03ece424cb09a1ac7024782c4be81
kit-foxboy <admin@vulpineinteractive.com>
2017-09-12 20:40:33 -0600

    All resolutions for drawables, added scoring methods

--- app/src/main/java/com/vulpineinteractive/animalmagnetism/QuizActivity.java
+++ app/src/main/java/com/vulpineinteractive/animalmagnetism/QuizActivity.java
@@ -1,13 +1,22 @@
 package com.vulpineinteractive.animalmagnetism;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class QuizActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        //call parent and init view
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        //init spinner
+        Spinner answer = (Spinner) findViewById(R.id.answerSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.answersArray, android.R.layout.select_dialog_item);
+        answer.setAdapter(adapter);
     }
 }
